a89b78dcf0c3867416b9e5ed3dd6927c7f975c4e
Aniruth <aniruth12@yahoo.com>
2020-12-13 15:34:41 +0530

    Working code

--- time.c
+++ time.c
@@ -13,10 +13,10 @@
 
 int main(int argc, char* argv[]) {
     double* ptr; // Shared memory pointer
-    const int SIZE = 4096;
+    const int SIZE = sizeof(double);
     int FD = shm_open("start_time", O_CREAT | O_RDWR, 0666);
+    ftruncate(FD, SIZE);
     ptr = (double*) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, FD, 0);
-    printf("Global: %p\n", ptr);
     pid_t pid;
     pid = fork();
 
@@ -25,23 +25,24 @@ int main(int argc, char* argv[]) {
     }
     else if (pid == 0) { // child process
         // write cur time into shared memory space
-        printf("Child: %p\n", ptr);
         struct timeval current;
         gettimeofday(&current, NULL);
-        // sprintf(ptr,"%lf", current.tv_sec + 1e-6 * current.tv_usec);
         *ptr = current.tv_sec + 1e-6 * current.tv_usec;
-        printf("Test: %s", ptr);
-        char* argv_list[] = { "ls" };
-        // execv("ls", argv_list);
-        sleep(1);
+        // running the command
+        if (argc > 1) {
+            const char* command = argv[1];
+            char** argv_list = (char**) malloc(argc * sizeof(char*));
+            for (int i = 0; i < argc - 1; i++) {
+                argv_list[i] = argv[i+1];
+            }
+            argv_list[argc - 1] = NULL;
+            execvp(command, argv_list);
+            // sleep(1);
+        }
     }
     else { // parent process
         wait(NULL);
-        // char* null;
-        // double start = *ptr;
-        double start = 0.0;
-        // printf("%s", ptr);
-        printf("Par: %p\n", ptr);
+        double start = *ptr;
         struct timeval current;
         gettimeofday(&current, NULL);
         printf("Execution Complete\nTime taken: %lf second(s)\n", current.tv_sec + 1e-6 * current.tv_usec - start);
