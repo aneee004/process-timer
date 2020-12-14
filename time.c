ad657810cdcd67c59c7da85f6d0eab1c443a1634
Aniruth <aniruth12@yahoo.com>
2020-12-14 23:58:40 +0530

    Cleaned

--- time.c
+++ time.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>	
-#include <string.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    double* ptr; // Shared memory pointer
-    const int SIZE = sizeof(double);
-    int FD = shm_open("start_time", O_CREAT | O_RDWR, 0666);
-    ftruncate(FD, SIZE);
-    ptr = (double*) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, FD, 0);
-    pid_t pid;
+    double* ptr; // shared memory pointer
+    struct timeval current_time; // to store the current time
+    pid_t pid; // to store the child process id
+    int SIZE, FD, i;
+
+    SIZE = sizeof(double); // size of the shared memory space
+    FD = shm_open("start_time", O_CREAT | O_RDWR, 0666); // opening the shared memory
+    ftruncate(FD, SIZE); // resizing the shared memory to the required size
+    ptr = (double*) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, FD, 0); // mapping the shared memory locaiton to the pointer
     pid = fork();
 
     if (pid < 0) { // fork failure
-        printf("Fork failed\n");
+        printf("Fork failure. Please try again.\n");
     }
     else if (pid == 0) { // child process
         // write cur time into shared memory space
-        struct timeval current;
-        gettimeofday(&current, NULL);
-        *ptr = current.tv_sec + 1e-6 * current.tv_usec;
+        gettimeofday(&current_time, NULL);
+        *ptr = current_time.tv_sec + 1e-6 * current_time.tv_usec;
         // running the command
         if (argc > 1) {
-            const char* command = argv[1];
-            char** argv_list = (char**) malloc(argc * sizeof(char*));
-            for (int i = 0; i < argc - 1; i++) {
-                argv_list[i] = argv[i+1];
+            for (int i = 1; i < argc; i++) {
+                argv[i-1] = argv[i];
             }
-            argv_list[argc - 1] = NULL;
-            execvp(command, argv_list);
-            // sleep(1);
+            argv[argc - 1] = NULL;
+            if (execvp(argv[0], argv) == -1) {
+                printf("Error running your command.\n");
+            }
+        }
+        else {
+            printf("No command to execute\n");
         }
     }
     else { // parent process
         wait(NULL);
-        double start = *ptr;
-        struct timeval current;
-        gettimeofday(&current, NULL);
-        printf("Execution Complete\nTime taken: %lf second(s)\n", current.tv_sec + 1e-6 * current.tv_usec - start);
+        gettimeofday(&current_time, NULL);
+        printf("\n\nElapsed Time: %lf second(s)\n", current_time.tv_sec + 1e-6 * current_time.tv_usec - *ptr);
     }
     shm_unlink("start_time");
     return 0;
